3d7db5c3a643e2370e386433e0241bc977077492
Beck Dehlsen <beckdehlsen@hotmail.com>
2018-09-23 14:46:13 -0700

    matching for default unit test

--- planets.py
+++ planets.py
@@ -1,5 +1,11 @@
 def weight_on_planets():
-   # write your code here
+   weightEarth = int(input("What do you weigh on earth? "))
+   weightMars = weightEarth * 0.38
+   weightJupiter = weightEarth * 2.34
+   print("\nOn Mars you would weigh", weightMars, "pounds.")
+   print("On Jupiter you would weigh", weightJupiter, "pounds.")
+   
+   return
    
    
    
